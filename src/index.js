23a0a564380d85caa9469c52c2e3bf5bcb082751
katsa9 <kathleen.brand2@gmail.com>
2021-09-02 20:54:33 +1000

    upgrade react scripts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { StylesProvider } from "@material-ui/core/styles";
+import 'react-app-polyfill/ie9'; // For IE 9-11 support
 
 import "./index.css";
 import App from "./App";
